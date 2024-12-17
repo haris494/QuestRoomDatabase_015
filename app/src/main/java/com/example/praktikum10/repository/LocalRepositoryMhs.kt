e4f5cb7e8dbec616a4d3ad5191e9874f319b7208
haris49 <hariis.459@gmail.com>
2024-12-17 16:15:44 +0700

    membuat kelas local repository mhs dengan ovveride

--- app/src/main/java/com/example/praktikum10/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/praktikum10/repository/LocalRepositoryMhs.kt
@@ -2,13 +2,30 @@ package com.example.praktikum10.repository
 
 import com.example.praktikum10.data.dao.MahasiswaDao
 import com.example.praktikum10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
-class LocalRepositoryMhs (
+class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
-) : RepositoryMhs{
+) : RepositoryMhs {
 
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
-        mahasiswaDao.insertMahasiswa(mahasiswa)
+        mahasiswaDao.insertMahasiswa(mahasiswa) // Perbaiki jika ada typo di sini
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim = nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
-
